906eb07ac1516ee64abfdb6336a244e5f255dd85
Walter Marquez <walteramg@gmail.com>
2018-06-30 12:31:18 -0500

    Alert prueba en archivo js

--- sites/all/modules/custom/bps_core/js/bps_core.js
+++ sites/all/modules/custom/bps_core/js/bps_core.js
@@ -54,7 +54,7 @@
     $('.cantidad').each(function(){
 		var id = $(this).attr('id');
 		var cantidad = $(this).val();
-		alert(id);
+		alert('Prueba');
 	});
   });
 /* Funcion que solo permite ingresar valores numericos */
