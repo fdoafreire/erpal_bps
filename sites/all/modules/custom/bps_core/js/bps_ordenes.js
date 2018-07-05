65198c47dbbfc409c257ca8263f492c942779c32
Walter Marquez <walteramg@gmail.com>
2018-07-05 00:01:33 -0500

    ativar campos ocultos bps_core.module class tipo_orden

--- sites/all/modules/custom/bps_core/js/bps_ordenes.js
+++ sites/all/modules/custom/bps_core/js/bps_ordenes.js
@@ -3,43 +3,43 @@
   $(document).ready(function(){
 		//Ocultos en ordenes de trabajo o servicio
 		var tipo = $("select[name='field_ord_tipo[und]'").val();
-		alert("tipo "+tipo);
 		if (tipo =="1"){
+			$('#node_ordenes_form_group_datos_el').hide()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').hide();
 			$('#node_ordenes_form_group_medicion_entrada').hide();
 			$('#node_ordenes_form_group_medicion_salida').hide();
 			$('#node_ordenes_form_group_ord_par_serv').show();
 		} else if (tipo=="2"){
+			$('#node_ordenes_form_group_datos_el').show()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').show();
 			$('#node_ordenes_form_group_medicion_entrada').show();
 			$('#node_ordenes_form_group_medicion_salida').show();
 			$('#node_ordenes_form_group_ord_par_serv').hide();
 		} else {
+			$('#node_ordenes_form_group_datos_el').hide()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').hide();
 			$('#node_ordenes_form_group_medicion_entrada').hide();
 			$('#node_ordenes_form_group_medicion_salida').hide();
 			$('#node_ordenes_form_group_ord_par_serv').hide();
 		}
-		/*$('#node_ordenes_form_group_datos_el').hide();
-		$('#node_ordenes_form_group_ord_datos_tecnicos').hide();
-		$('#node_ordenes_form_group_medicion_entrada').hide();
-		$('#node_ordenes_form_group_medicion_salida').hide();
-		$('#node_ordenes_form_group_ord_par_serv').hide();*/
   });
   $(document).on('change','.tipo_orden', {} ,function(e){
 		var id_tipo = $(e.currentTarget).attr('id');
 		var tipo = $("#"+id_tipo+" option:selected").val();
 		if (tipo =="1"){
+			$('#node_ordenes_form_group_datos_el').hide()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').hide();
 			$('#node_ordenes_form_group_medicion_entrada').hide();
 			$('#node_ordenes_form_group_medicion_salida').hide();
 			$('#node_ordenes_form_group_ord_par_serv').show();
 		} else if (tipo=="2"){
+			$('#node_ordenes_form_group_datos_el').show()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').show();
 			$('#node_ordenes_form_group_medicion_entrada').show();
 			$('#node_ordenes_form_group_medicion_salida').show();
 			$('#node_ordenes_form_group_ord_par_serv').hide();
 		} else {
+			$('#node_ordenes_form_group_datos_el').hide()
 			$('#node_ordenes_form_group_ord_datos_tecnicos').hide();
 			$('#node_ordenes_form_group_medicion_entrada').hide();
 			$('#node_ordenes_form_group_medicion_salida').hide();
