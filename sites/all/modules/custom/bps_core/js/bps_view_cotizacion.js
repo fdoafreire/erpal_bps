0d25464a5ba4556370758bfbd4dc1c2d4d8b3bd0
Walter Marquez <walteramg@gmail.com>
2018-07-17 22:23:52 -0500

    totales consulta cotizacion valoriva

--- sites/all/modules/custom/bps_core/js/bps_view_cotizacion.js
+++ sites/all/modules/custom/bps_core/js/bps_view_cotizacion.js
@@ -66,51 +66,15 @@
 									'<tr>'+
 										'<td></td>'+
 										'<td></td>'+
-										'<td></td>'+
+										'<td>'+cantidad+'</td>'+
 										'<td></td>'+
 										'<td>'+sub+'</td>'+
 										'<td></td>'+
 										'<td></td>'+
 										'<td>'+iva+'</td>'+
 										'<td>'+total+'</td>'+
-									'</tr>';
-									
+									'</tr>';									
 		$('table.field-collection-view-final tbody').append(totales);
-    
-    var subtotal_cot = 0;
-    var total_cantidad = 0;
-    var total_dcto = 0;
-    var total_iva = 0;
-    var total_cotizacion = 0;
-    $('.cantidad').each(function(){
-			var id_cant = $(this).attr('id');
-			var id_valor = id_cant.replace('ref-cantidad', 'valor-unitario');
-			var id_dscto = id_cant.replace('ref-cantidad', 'descuento');
-			var id_iva = id_cant.replace('ref-cantidad', 'porcentaje-impuesto');
-			var id_vlriva = id_cant.replace('ref-cantidad', 'valor-iva');
-			var id_total = id_cant.replace('ref-cantidad', 'total');
-			var cant  = parseFloat($('#' + id_cant).val());
-			var valor = parseFloat($('#' + id_valor).val());
-			var dscto = parseFloat($('#' + id_dscto).val());						
-			var iva   = parseFloat($('#' + id_iva).val());
-
-			var subtotal = (cant * valor) - ((cant * valor * dscto) / 100);
-			var descuento = (cant * valor * dscto) / 100;
-			var valor_iva = (subtotal * iva) / 100;
-			var total = subtotal + valor_iva;
-			
-			if (subtotal >0){
-				total_cantidad = parseFloat(total_cantidad) + parseFloat(cant);
-				subtotal_cot = parseFloat(subtotal_cot) + parseFloat(subtotal);
-				total_dcto = parseFloat(total_dcto) + parseFloat(descuento);
-				total_iva = parseFloat(total_iva) + parseFloat(valor_iva);
-				total_cotizacion = parseFloat(total_cotizacion) + parseFloat(total);
-			}
-		});
-		$('#edit-subtotal').val(subtotal_cot);	
-		$('#edit-descuentos').val(total_dcto);	
-		$('#edit-total-iva').val(total_iva);	
-		$('#edit-total-cotizacion--2').val(total_cotizacion);	  
   });
 
 }(jQuery));
