f504237bd7547972e746ca43b5d33ea57f52d68b
Fernando Freire <fdoafreire@gmail.com>
2018-08-11 17:11:49 -0500

    ajusytes

--- sites/all/modules/custom/bps_core/js/bps_equipo_laboratorio.js
+++ sites/all/modules/custom/bps_core/js/bps_equipo_laboratorio.js
@@ -1,7 +1,6 @@
 (function ($) {
 
   populate_ordenes = function( event, ui,id_element,id_selected ) {
-    alert('hi');
     $('#field_el_cliente_id').val(id_selected);
     $("#" + id_element).find('option').remove().end().append('<option value="0" selected="selected">- Seleccionar -</option>');
     $.getJSON('/bps/ajax/ordenes/' + id_selected, function(data){
