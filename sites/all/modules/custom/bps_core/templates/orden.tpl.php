f9a00fe7612cf898214641658e8849c2edf49a64
Walter Marquez <walteramg@gmail.com>
2018-08-11 23:58:33 -0500

    PDF ORDENES

--- sites/all/modules/custom/bps_core/templates/orden.tpl.php
+++ sites/all/modules/custom/bps_core/templates/orden.tpl.php
@@ -493,7 +493,7 @@
 					<caption style="border:1px solid;font-size:13px;font-weight:bold;">INFORME TECNICO</caption>
 					<tbody>
 						<tr>
-							<td style="width:900px;height:100px;text-align:center;border:1px solid;font-size:13px;padding:0;"><?php print $informet;?></td>
+							<td style="width:900px;height:100px;text-align:left;border:1px solid;font-size:13px;padding:0;;vertical-align:top;"><?php print $informet;?></td>
 						</tr>
 					</tbody>
 				</table>
@@ -502,7 +502,7 @@
 					<caption style="border:1px solid;font-size:13px;font-weight:bold;">RECOMENDACIONES</caption>
 					<tbody>
 						<tr>
-							<td style="width:900px;height:100px;text-align:center;border:1px solid;font-size:13px;padding:0;"><?php print $recomendaciones;?></td>
+							<td style="width:900px;height:100px;text-align:left;border:1px solid;font-size:13px;padding:0;vertical-align:top;"><?php print $recomendaciones;?></td>
 						</tr>
 					</tbody>
 				</table>
