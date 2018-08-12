99cf03067d9a28e2ba28bb115fccaf77906905a3
Walter Marquez <walteramg@gmail.com>
2018-08-12 14:49:20 -0500

    PDF ORDENES

--- sites/all/modules/custom/bps_core/templates/laboratorio.tpl.php
+++ sites/all/modules/custom/bps_core/templates/laboratorio.tpl.php
@@ -26,7 +26,7 @@
   $contact = '';
   $email = '';
 
-	$item = field_collection_item_load($node->field_el_orden['und'][0]['target_id']);
+	$item = node_load($node->field_el_orden['und'][0]['target_id']);
 	
   if (isset($node->field_ord_sucursal_id['und'][0]['value'])) {
   	$item_orden = field_collection_item_load($node->field_ord_sucursal_id['und'][0]['value']);
@@ -110,7 +110,7 @@
 					<th style="width:225px;font-size:15px;padding:0;text-align:center;border:1px solid;">SERIAL</th>
 				</tr>
 				<tr style="height:130px;">
-					<td style="width:225px;font-size:15px;padding:0;text-align:left;border:1px solid;"><?php print $node->field_el_orden['und'][0]['target_id'];?></td>
+					<td style="width:225px;font-size:15px;padding:0;text-align:left;border:1px solid;"><?php print $item->title;?></td>
 					<td style="width:225px;font-size:15px;padding:0;text-align:left;border:1px solid;">colocar la referncia del equipo</td>
 					<td style="width:225px;font-size:15px;padding:0;text-align:left;border:1px solid;">colocar marca del equipo</td>
 					<td style="width:225px;font-size:15px;padding:0;text-align:left;border:1px solid;">colocar serial del equipo</td>
@@ -127,7 +127,7 @@
 				</tr>
 				<tr>
 					<td style="width:200px;font-size:15px;padding:0;text-align:left;border:1px solid;"><?php print $dpto;?></td>
-					<td style="width:200px;height:130px;font-size:15px;padding:0;text-align:left;border:1px solid;">colocar la referncia del equipo</td>
+					<td style="width:200px;height:130px;font-size:15px;padding:0;text-align:left;border:1px solid;"></td>
 				</tr>
 			</tbody>
 		</table>
