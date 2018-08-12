0524e6f1bd811b4f35eda0321eaf2ab522261a78
Walter Marquez <walteramg@gmail.com>
2018-08-12 14:26:58 -0500

    PDF ORDENES

--- sites/all/modules/custom/bps_core/templates/laboratorio.tpl.php
+++ sites/all/modules/custom/bps_core/templates/laboratorio.tpl.php
@@ -25,7 +25,7 @@
   $email = '';
 
   $orden = node_load($node->field_el_orden['und'][0]['target_id']);
-  $orden_equipo = $node->field_el_orden['und'][0]['value'];
+  $orden_equipo = $node->field_el_orden['und'][0]['target_id'];
 
 
   if (isset($node->field_ord_sucursal_id['und'][0]['value'])) {
