21db928c621deacb7df5e206c7e5f64e4dc4d50d
Walter Marquez <walteramg@gmail.com>
2018-10-06 09:18:18 -0500

    MONEDA EN PDF COTIZACIONES

--- sites/all/modules/custom/bps_core/templates/cotizacion.tpl.php
+++ sites/all/modules/custom/bps_core/templates/cotizacion.tpl.php
@@ -111,7 +111,7 @@
 		<table cellpadding="0" cellspacing="0" style="border-collapse:collapse;border-spacing=0;">
 			<tbody>
 				<tr>
-					<td style="width:230px;font-size:20px;padding:0;text-align:center;"><?php print $img_logo." ".$name_money;?></td>
+					<td style="width:230px;font-size:20px;padding:0;text-align:center;"><?php print $img_logo;?></td>
 					<td style="width:300px;font-size:20px;font-weight:bold;padding:0;text-align:center;">COTIZACION</td>
 				</tr>
 			</tbody>
